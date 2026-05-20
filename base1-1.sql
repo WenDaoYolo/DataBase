01730dd82ee9877c6a3b2b0a1ad613d71c327a69
WenDaoYolo <3547474131@qq.com>
2026-05-20 22:05:35 +0800

    Add files via upload

---
+++ base1-1.sql
@@ -0,0 +1,12 @@
+create table student(
+    id int,
+    name varchar(10),
+    sex varchar(10),
+    address varchar(20)
+);
+
+insert into student (id,name,sex,address) values
+    (101,'zhangsan','male','Hubei-Yichang'),
+    (102,'lisi','female','Hubei-Wuhan'),
+    (103,'wangwu','male','Hunan-Changsha'),
+    (104,'xiaoliu','female','Hubei-Xiaogan');
